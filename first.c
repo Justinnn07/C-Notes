c0dd3163a1275f1408b6b3a567db1093b03a2642
Justinnn07 <justinv2006@gmail.com>
2020-12-27 23:59:45 +0530

    learnt how to take input from the user

--- first.c
+++ first.c
@@ -1,9 +1,8 @@
-# include <stdio.h>
+#include <stdio.h>
 
 int main() {
-    int tom;
-    int Tom;
-    // "@";
-    printf("HELLO WORLD");
-    return 0;
+	int a;
+	int b;
+	printf("I AM JUSTIN");
+	return 0;
 }
---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 4;
+    int k = 8;
+    float b = 8.5;
+    char c = 'u';
+    // printf("The value of a is %d %c \n", a, c);
+    // printf("The value of a is %d %c \n", a, c);
+    // printf("The value of a is %d %f \n", a, b);
+    printf("SUM is = %d", k+a );
+    return 0;
+}       
