52641116fb3f07f1cc25f2463133a4082a97c698
Justinnn07 <justinv2006@gmail.com>
2020-12-28 22:32:16 +0530

    added a test and a progress.md

--- test/test-input.c
+++ test/test-input.c
@@ -3,9 +3,9 @@
 int main() {
     int a,b;
     printf("1st no. to be added \n");
-    scanf("%d", &a);
+    scanf("%d", &a); // important
     printf("2nd no. to be added \n");
-    scanf("%d", &b);
+    scanf("%d", &b); // important
     printf("THE VALUE IS %d", a+b);
     return 0;
 }
