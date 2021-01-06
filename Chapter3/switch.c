08bcd34b509f7e1a2c4e9f3ea041c6ccae92d73f
Justinnn07 <justinv2006@gmail.com>
2021-01-06 23:45:27 +0530

    switch

---
+++ Chapter3/switch.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+
+int main() { 
+    int rating;
+    printf("ENTER YOUR RATING (1-5) ");
+    scanf("%d", &rating);
+    switch(rating){
+        case 1:
+        printf("YOUR RATING IS 1 ");
+        break;
+        case 2: 
+        printf("YOUR CASE IS 2 ");
+        break;
+        case 3: 
+        printf("YOUR CASE IS 3 ");
+        break;
+        case 4: 
+        printf("YOUR CASE IS 4 ");
+        break;
+        default: 
+        printf("INVALID");
+    };
+    return 0;
+}
