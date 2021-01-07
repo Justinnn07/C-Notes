45d4ea79d15c53bb0db495d28f61d8333984eeb8
Justinnn07 <justinv2006@gmail.com>
2021-01-08 03:57:41 +0530

    finsished practice set

---
+++ Chapter3/practice sets/income-tax.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main() {  
+    float tax = 0, income;
+    printf("ENTER YOUR INCOME ");
+    scanf("%f", &income);
+    if (income >= 250000 && income <= 500000){
+        tax = tax + 0.05 * (income - 250000);
+    }
+       if (income >= 500000 && income <= 500000){
+        tax = tax + 0.25 * (income - 1000000) ;
+    }
+       if (income >= 1000000){
+        tax = tax + 0.05 * (income - 1000000 );
+    }
+    printf("YOUR TAX TO BE PAID IS %f", tax);
+     return 0;
+}
